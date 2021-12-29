d6afe62480b5b3911dc38286829e89773dddb624
Pierre Minier <pierre.minier@bordeaux-inp.fr>
2021-12-29 18:57:51 +0100

    [Découpage] 0-padding pour toujours avoir des blocs 8x8

--- compression/decoupage.m
+++ compression/decoupage.m
@@ -2,7 +2,7 @@ function blocs = decoupage(YCbCr)
 %DECOUPAGE(YCbCr) découpe la luminance et les chrominances en bloc de 8x8
     blocs = cell(3, 1);
     for i = 1:3
-        blocs{i} = creerBloc(YCbCr{i});
+        blocs{i} = creerBloc(padding8(YCbCr{i}));
     end
 end
 
@@ -14,4 +14,13 @@ function bloc = creerBloc(canal)
             bloc{i, j} = canal((i-1)*8+1:i*8, (j-1)*8+1:j*8);
         end
     end
+end
+
+function canal = padding8(canal)
+%PADDING8 effectue un 0-padding pour que les dimensions du canal soient multiples de 8
+    [h, w] = size(canal);
+    h0 = (mod(h, 8) ~= 0) * (8 - mod(h, 8));
+    w0 = (mod(w, 8) ~= 0) * (8 - mod(w, 8));
+    canal = [canal; zeros(h0, w)];
+    canal = [canal zeros(h+h0, w0)];
 end
