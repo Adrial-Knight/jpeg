a23aff412be316ec03f917593a675e52f6b23fe6
Pierre Minier <pierre.minier@bordeaux-inp.fr>
2021-12-30 19:12:57 +0100

    [Huffman] plus rapide

--- compression/huffman.m
+++ compression/huffman.m
@@ -24,41 +24,29 @@ function [code, table] = huffman(mot)
         frequence{i, 2} = table(clefs{i});
     end
     occurence = frequence; % utilisé plus tard pour connaitre la mémoire à allouer pour le codage du mot
+    frequence = trie(frequence, 2);
 
-%% construction de l'arbre
-    arbre = cell(1, n-1);
-    arbre{1} = trie(frequence, 2); % les feuilles de l'arbre
+%% construction de la table de codage
+    % nettoyage de la table
+    for i = 1:n
+        table(clefs{i}) = [];
+    end
     for i = 1:n-1
-        frequence{n-i, 1} = strcat(strcat(frequence{n-i+1, 1}, ","), frequence{n-i, 1}); % fusion
+        for j = 0:1
+            lettres = strsplit(frequence{n-i+j, 1}{:}, ","); % les lettres du fils de gauche (n-i) et du droit (n-1+1)
+            for k = 1:length(lettres)
+                table(lettres{k}) = [j table(lettres{k})];
+            end
+        end
+        frequence{n-i, 1} = strcat(strcat(frequence{n-i, 1}, ","), frequence{n-i+1, 1}); % fusion
         frequence{n-i, 2} = frequence{n-i+1, 2} + frequence{n-i, 2}; % somme des fréquences d'apparition
         frequence = frequence(1:n-i, :); % suppression de la dernière ligne
         if (i < n-1)
             frequence = trie(frequence, 2);
         end
-        arbre{i+1} = frequence; % ajout d'un étage à l'arbre
     end
 
-%% parcours de l'arbre pour remplir la table
-    % nettoyage de la table
-    for i = 1:n
-        table(clefs{i}) = [];
-    end
-    % parcours de la racine (n-1) vers les feuilles (1)
-    for i = n-1:-1:1
-        if i < n-1
-            indice = indiceFusion(arbre{i+1}, arbre{i}); % indices des mots qui ont été fusionnés
-        else
-            indice = [1 2]; % la racine possède 2 fils, aux indices 1 et 2 de l'étage inférieur
-        end
-        for j = 1:2
-            lettres = strsplit(arbre{i}{indice(j), 1}{1}, ","); % les lettres du mot considéré
-            for k = 1:length(lettres)
-                table(lettres{k}) = [table(lettres{k}) j-1];
-            end
-        end
-    end
-
-%% codage du mot renseigné
+%% codage du mot
     % évaluation de la mémoire nécessaire
     memoire = 0;
     for i = 1:length(occurence)
@@ -93,23 +81,4 @@ function tab = trie(tab, i)
 end
 
 function [a, b] = echange(b, a)
-end
-
-function indice = indiceFusion(etage, etageInf)
-%INDICEFUSION(etage, etageInf) recherche les deux mots de l'étage inférieur qui ont été fusionnés.
-    indice = [-1 -1]; % on a une erreur si ces deux valeurs n'ont pas été remplacées
-    ii = 1;
-    for i = 1:length(etageInf)
-        detect = false;
-        for j = 1:length(etage);
-            if(strcmp(etageInf{i}{1}, etage{j}{1}))
-                detect = true;
-                break;
-            end
-        end
-        if(~detect)
-            indice(ii) = i;
-            ii = 2;
-        end
-    end
 end
